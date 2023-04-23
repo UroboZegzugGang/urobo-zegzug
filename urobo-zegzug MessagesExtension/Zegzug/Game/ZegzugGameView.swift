f7a4647e3c7fded7b915fdaa41dacad9d8f53dac
Szabo Tamas <tamas.szabo0102@gmail.com>
2023-04-23 23:48:30 +0200

    Add buttons to game view

--- urobo-zegzug MessagesExtension/Zegzug/Game/ZegzugGameView.swift
+++ urobo-zegzug MessagesExtension/Zegzug/Game/ZegzugGameView.swift
@@ -2,6 +2,28 @@ import SwiftUI
 
 struct ZegzugGameView: View {
     var body: some View {
-        BackgroundCircles()
+        VStack {
+            resetButton()
+            BackgroundCircles()
+            sendButton()
+        }
+    }
+
+    @ViewBuilder private func resetButton() -> some View {
+        HStack {
+            Spacer()
+            Button("Reset") {
+                // TODO: reset current turn
+            }
+        }
+        .padding(.trailing)
+    }
+
+    @ViewBuilder private func sendButton() -> some View {
+        Button("Send") {
+            // TODO: End turn and send it
+        }
+        .buttonStyle(.borderedProminent)
+        .padding()
     }
 }
